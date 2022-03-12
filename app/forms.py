ede3e30af5be97ad1494e6ef6c714534965266c7
Matthew Hays <hays8786@gmail.com>
2022-03-12 08:13:56 -0500

    Chose Color Palette

--- app/forms.py
+++ app/forms.py
@@ -4,4 +4,5 @@ from wtforms.validators import DataRequired
 
 class PrimarySearchForm(FlaskForm):
     user_query = StringField('', validators=[DataRequired()])
+    submit = SubmitField('Submit')
    
