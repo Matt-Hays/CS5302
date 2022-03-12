2cfb55789e08210f38ad4d5a43436bde3c32a21a
Matthew Hays <hays8786@gmail.com>
2022-03-11 19:22:24 -0500

    Add Basic Navigation

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config(object):
     SECRET_KEY = "myamazinglycomplexsecretkeythatnoonewilleverguess"
     SQLALCHEMY_DATABASE_URI = dotenv_values(".env.CONNECTION_STRING")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # TESTING = True
