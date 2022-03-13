d19d3ddf3557d2499876db20f749fe6ce578ac17
Matthew Hays <hays8786@gmail.com>
2022-03-12 23:19:01 -0500

    Add New 2019 Lahman DB, Connect Player Search Bar to DB through Analysis Table

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
-from crypt import methods
 from app import app
 from flask import render_template, flash, redirect
 from app.forms import PrimarySearchForm
+from app.models import Analysis
 
 
-@app.route("/", methods=['GET', 'POST'])
-@app.route("/index", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 def index():
     form = PrimarySearchForm()
     if form.validate_on_submit():
-        print(form.user_query.data)
+        results = Analysis.query.filter_by(playerid=form.user_query.data).all()
+        return render_template("index.html", form=form, results=results)
     return render_template("index.html", form=form)
