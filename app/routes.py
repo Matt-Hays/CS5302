957d9ad9c3941d4b2dd3237132d30d725a03be30
Matthew Hays <83807309+Matt-Hays@users.noreply.github.com>
2022-03-31 11:21:38 -0400

    5 create a login system (#18)
    * Primary Form and Slight Styling
    
    * Chose Color Palette
    
    * Navbar Complete Basic Styles
    
    * Merge with 4-player-display
    
    * Add New 2019 Lahman DB, Connect Player Search Bar to DB through Analysis Table
    
    * Search by Playerid Connection and Basic View
    
    * Fix Pre-Commit Hooks
    
    * Add Login Workflows
    
    * Add User Table and Fresh DB SQL Script
    
    * Fix JS Logic, Add Account Page, RM Secret.html

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,82 @@
-from app import app
-from flask import render_template
+from flask import redirect, render_template, url_for, flash, request
+from app import app, db
+from werkzeug.urls import url_parse
+from flask_login import current_user, login_required, login_user, logout_user
+from app.models import User
+from app.forms import LoginForm, RegistrationForm
 
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+
+@app.route("/players")
+def players():
+    return render_template("players.html", title="Players Page")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Redirect if the user is authenticated already
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+    form = LoginForm()
+
+    # If form data passes validation, find the requested user
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+
+        # If no user found or the assword is incorrect,
+        # return to login page with flashed message
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+
+        # If all validations pass, login the user
+        login_user(user, remember=form.remember_me.data)
+
+        # If user attempted an authroized route prior to login,
+        # return user to that route after successful login
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page) != "":
+            next_page = url_for("account")
+
+        return redirect(next_page)
+
+    # Else, return login.html
+    return render_template("login.html", title="Login Page", form=form)
+
+
+# Logout user route; must be logged in to logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+
+# Protected route example
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Secret Page")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        # BEGIN TRANSACTION #
+        db.session.add(user)
+        db.session.commit()
+        # END TRANSACTION #
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
