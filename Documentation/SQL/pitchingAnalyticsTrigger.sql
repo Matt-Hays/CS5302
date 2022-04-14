b7bb0c0cd65d6d84d84f9800ea696efb419365b7
ronwellman <ron.wellman01@gmail.com>
2022-04-14 18:22:33 -0400

    Fixed tabs and spaces issue in pitchingAnalyticsTrigger.sql.

--- Documentation/SQL/pitchingAnalyticsTrigger.sql
+++ Documentation/SQL/pitchingAnalyticsTrigger.sql
@@ -16,10 +16,10 @@ UPDATE PitchingAnalytics SET
     PA = NEW.AB+NEW.BB+NEW.HBP+NEW.SF+NEW.SH,
     RC =
         CASE -- avoiding division by 0
-				WHEN PA IS NOT NULL AND PA <> 0 THEN (TOB*BA)/PA
-                WHEN PA IS NULL THEN NULL
-		ELSE 0
-		END,
+            WHEN PA IS NOT NULL AND PA <> 0 THEN (TOB*BA)/PA
+            WHEN PA IS NULL THEN NULL
+        ELSE 0
+    END,
     PARC = RC/(((SELECT BPF FROM teams WHERE yearID = NEW.yearID AND teamID = NEW.teamID)+100)/200),
     PARC27 = (PARC*27)/(NEW.AB+NEW.SF+NEW.SH+NEW.CS+NEW.GIDP-NEW.H),
     PARCA = RC/(((SELECT PPF FROM teams WHERE yearID = NEW.yearID AND teamID = NEW.teamID)+100)/200)
